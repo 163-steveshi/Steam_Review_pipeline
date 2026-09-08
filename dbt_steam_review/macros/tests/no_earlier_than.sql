f66fe4a370dcfdee67ecd3a7586284c39a6c9cc5
ShangShi <ShangShi@cmail.carleton.ca>
2026-09-08 17:53:03 -0400

    wrap up the scd1 review table test

--- dbt_steam_review/macros/tests/no_earlier_than.sql
+++ dbt_steam_review/macros/tests/no_earlier_than.sql
@@ -1,6 +1,6 @@
 {% macro no_earlier_than(column_name_1, column_name_2) %}
     {{ column_name_1 }} is not null 
     AND {{ column_name_2 }} is not null
-    AND  {{ column_name_1 }} < {{column_name_2}}
+    AND  {{ column_name_1 }} > {{column_name_2}}
     
 {% endmacro %}
--- dbt_steam_review/tests/generic/no_earlier_than_test.sql
+++ dbt_steam_review/tests/generic/no_earlier_than_test.sql
@@ -1,5 +1,5 @@
 {% test no_earlier_than_test(model, column_name_1, column_name_2) %}
     select *
     from {{ model }}
-    where {{ no_earlier_than_test(column_name_1, column_name_2) }}
+    where {{ no_earlier_than(column_name_1, column_name_2) }}
 {% endtest %}
